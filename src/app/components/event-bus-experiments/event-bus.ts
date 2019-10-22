6d5fe313eefba8959208f4c605a8aadd3534df4c
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-10-22 16:38:35 +0200

    Create Subject Interface

--- src/app/components/event-bus-experiments/event-bus.ts
+++ src/app/components/event-bus-experiments/event-bus.ts
@@ -3,3 +3,9 @@ export interface Observer {
   notify(data: any);
 }
 
+interface Subject {
+  registerObserver(eventType: string, obs: Observer);
+  unregisterObserver(eventType: string, obs: Observer);
+  notifyObservers(eventType: string, data: any);
+}
+
