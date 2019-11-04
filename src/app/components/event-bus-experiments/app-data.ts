e27364fb72d40ba9d2e1e31b17e2263138cfbd6d
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-04 18:23:14 +0100

    Refactor DataStore for use RxJS

--- src/app/components/event-bus-experiments/app-data.ts
+++ src/app/components/event-bus-experiments/app-data.ts
@@ -2,61 +2,16 @@ import { Lesson } from './../../shared/model/lesson';
 
 import * as _ from 'lodash';
 
-export interface Observer {
-  // Ability of emitting new data and being notified
-  next(data: any);
-}
-
-export interface Observable {
-  // Ability to subscribe to new data
-  subscribe(obs: Observer);
-  unsubscribe(obs: Observer);
-}
-
-// The Subject essentially is the "EventBus"
-interface Subject extends Observer, Observable {
-}
-
-class SubjectImplementation implements Subject {
-
-  private observers: Observer[] = [];
-
-  next(data: any) {
-    // Breoadcate to all observers the emitted data
-    this.observers.forEach(
-      obs => obs.next(data)
-    );
-  }
-
-  subscribe(obs: Observer) {
-    this.observers.push(obs);
-  }
-
-  unsubscribe(obs: Observer) {
-    _.remove(
-      this.observers,
-      el => el === obs
-    );
-  }
-
-}
+import { Subject, Observable, Observer } from 'rxjs';
 
 // Centralized service that manages the data
 class DataStore {
 
  private lessons: Lesson[] = [];
 
- private lessonsListSubject = new SubjectImplementation();
+ private lessonsListSubject = new Subject();
 
- public lessonsList$: Observable = {
-  subscribe: obs => {
-    this.lessonsListSubject.subscribe(obs);
-    obs.next(this.lessons);
-  },
-  unsubscribe: obs => {
-    this.lessonsListSubject.unsubscribe(obs);
-  }
-};
+ public lessonsList$: Observable<Lesson[]>  = this.lessonsList$.asObservable();
 
  public initializeLessonsList(newList: Lesson[]) {
     this.lessons = _.cloneDeep(newList);
